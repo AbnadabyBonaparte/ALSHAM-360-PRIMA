86c05ad133c1940e8c74d38a0a751f98bd9fa300
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-09-09 09:23:59 -0300

    Delete src directory

--- src/js/relacionamentos.js
+++
@@ -1,5 +0,0 @@
-// Módulo src/js/relacionamentos.js
-console.log("✅ Módulo de Relacionamentos carregado.");
-
-// A lógica futura para a página de relacionamentos irá aqui.
-// Por enquanto, este arquivo existe para satisfazer o processo de build do Vite.
