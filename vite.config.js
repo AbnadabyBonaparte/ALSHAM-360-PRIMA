5b6876ac1c1af5be4b442c190bdb3e6a747f4084
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-10-31 06:40:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    target: 'es2022', // ATIVA top-level await
     rollupOptions: {
       input: {
         main: './index.html',
@@ -10,6 +11,11 @@ export default defineConfig({
         'init-supabase': './src/lib/init-supabase.js',
         'attach-supabase': './src/lib/attach-supabase.js'
       }
+    }
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true
     }
   }
 });
