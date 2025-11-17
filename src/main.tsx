53ea0adf464644057c1e1d81ca81bcbd60b046b8
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-11-17 09:35:29 -0300

    feat: import compact.css for global size adjustments

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './themes.css'
 import './responsive.css'
+import './compact.css'  // ← ADICIONAR ESTA LINHA!
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
