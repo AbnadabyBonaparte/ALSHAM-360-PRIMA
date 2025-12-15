fba05a031966112753ff01d3db8aba6610a45799
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-12-15 04:52:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,32 +47,38 @@ import { Bar, Doughnut, Line } from "react-chartjs-2";
 import campaignOrion from "./assets/campaign-orion.png";
 import { registerRoute, renderPage, resolveRouteOrDefault } from "./routes";
 
-// Páginas - todos como named exports (para evitar erro "default is not exported")
+// Páginas existentes e reais (com named exports)
 import { Leads } from "./pages/Leads";
 import { LeadsDetails } from "./pages/LeadsDetails";
 import { DashboardSupremo } from "./pages/Dashboard";
-import { Customer360 } from "./pages/Customer360";
-import { AutomationBuilder } from "./pages/AutomationBuilder";
 import { Pipeline } from "./pages/Pipeline";
 import { OmnichannelInbox } from "./pages/OmnichannelInbox";
-import { NFTGallery } from "./pages/NFTGallery";
-import { WarArena } from "./pages/WarArena";
-import { ThemeSwitcher } from "@/components/ThemeSwitcher";
-import { LandingPageBuilder } from "./pages/LandingPageBuilder";
-import { ExecutiveDashboard } from "./pages/ExecutiveDashboard";
-import { Metaverse } from "./pages/Metaverse";
-import { AlshamOS } from "./pages/AlshamOS";
-import { TheBoardroomOmega } from "./pages/TheBoardroomOmega";
-import { AIAssistant } from "./pages/AIAssistant";
-import { Achievements } from "./pages/Achievements";
-import { SidebarDesktop } from "./components/SidebarSupremo";
 import { Analytics } from "./pages/Analytics";
-import { Automacoes } from "./pages/Automacoes";
-import { Financeiro } from "./pages/Financeiro";
-import { Gamificacao } from "./pages/Gamificacao";
-import { Publicacao } from "./pages/Publicacao";
-import { Seguranca } from "./pages/Seguranca";
+import { Automations } from "./pages/Automations";
+import { Gamification } from "./pages/Gamification";
+
+// Componentes
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { SidebarDesktop } from "./components/SidebarSupremo";
 import { useUrlSync } from "./hooks/useUrlSync";
+
+// Páginas que ainda não existem ou não têm export correto (comentadas temporariamente)
+ // import { Customer360 } from "./pages/Customer360";
+ // import { AutomationBuilder } from "./pages/AutomationBuilder";
+ // import { NFTGallery } from "./pages/NFTGallery";
+ // import { WarArena } from "./pages/WarArena";
+ // import { LandingPageBuilder } from "./pages/LandingPageBuilder";
+ // import { ExecutiveDashboard } from "./pages/ExecutiveDashboard";
+ // import { Metaverse } from "./pages/Metaverse";
+ // import { AlshamOS } from "./pages/AlshamOS";
+ // import { TheBoardroomOmega } from "./pages/TheBoardroomOmega";
+ // import { AIAssistant } from "./pages/AIAssistant";
+ // import { Achievements } from "./pages/Achievements";
+ // import { Automacoes } from "./pages/Automacoes";
+ // import { Financeiro } from "./pages/Financeiro";
+ // import { Gamificacao } from "./pages/Gamificacao";
+ // import { Publicacao } from "./pages/Publicacao";
+ // import { Seguranca } from "./pages/Seguranca";
 
 ChartJS.register(
   CategoryScale,
