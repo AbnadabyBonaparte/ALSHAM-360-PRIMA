c3e7bc7b83b44fb3a4ac245941fdd19ef52e4422
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-12-15 04:43:37 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./index.css"  // ← APENAS ESTE! (já inclui tudo)
-import App from './App.tsx"
+import "./index.css" // ← APENAS ESTE! (já inclui tudo)
+import App from './App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
